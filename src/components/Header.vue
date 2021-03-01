6c9934ed3866bd151b091fb7e0be61b2f9474772
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2021-03-01 20:42:05 +0600

    first

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>ХУЙ</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style lang="sass">
+    h1
+        color: black
+</style>

9cb97665f0063e577f82ac1b17f29edf895b9ded
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2021-03-10 13:19:28 +0600

    fx: adaptive

--- src/components/Associate.vue
+++ src/components/Associate.vue
@@ -21,6 +21,24 @@
         <div class="associate_image_phone">
             <img src="../assets/img-4 mobile.png" alt="img">
         </div>
+
+
+        <div class="associate_background_images">
+            <img class="associate_background_images_1" src="../assets/back/telegram_back.png" alt="telegram_back">
+            <img class="associate_background_images_2" src="../assets/back/vk_back.png" alt="vk_back">
+            <img class="associate_background_images_3" src="../assets/back/whatsapp_back_2.png" alt="whatsapp_back_2">
+            <img class="associate_background_images_4" src="../assets/back/viber_back.png" alt="yellowby">
+            <img class="associate_background_images_5" src="../assets/back/purplesn.png" alt="purplesn">
+            <img class="associate_background_images_6" src="../assets/back/greymy.png" alt="greenby">
+            <img class="associate_background_images_7" src="../assets/back/lightbluemy.png" alt="lightbluemy">
+            <img class="associate_background_images_8" src="../assets/back/greenby.png" alt="greenby">
+            <img class="associate_background_images_9" src="../assets/back/purpleby.png" alt="purpleby">
+            <img class="associate_background_images_10" src="../assets/back/bluemn.png" alt="bluemn">
+            <img class="associate_background_images_11" src="../assets/back/greysn.png" alt="greysn">
+            <img class="associate_background_images_12" src="../assets/back/purplebyhalf.png" alt="greysn">
+        </div>
+
+
     </div>
 </template>
 
@@ -33,6 +51,7 @@ export default {
 <style lang="sass">
 @media screen and (min-width: 456px)
     .associate
+        margin-top: 100px
         .associate_nav
             display: inline-block
             width: 47.3%
@@ -86,6 +105,7 @@ export default {
             vertical-align: top
             position: relative
             top: -30px
+            margin-bottom: 50px
             img 
                 transition: 0.5s all ease
                 position: absolute
@@ -94,56 +114,50 @@ export default {
         .associate_image_phone
             display: none
         .associate_background_images
+            position: absolute
+            height: 0px
             img
                 position: relative
             .associate_background_images_1
-                top: 0px
-                left: 950px
+                top: -60px
+                left: 500px
                 z-index: 2
             .associate_background_images_2
-                left: -120px
-                top: -200px
-            .associate_background_images_3
-                top: -400px
-                left: 154px
-            .associate_background_images_4
-                top: -270px
-                z-index: 1 
-                left: 220px
-            .associate_background_images_5
-                position: absolute
+                left: 730px
+                top: -830px
                 z-index: 2
-                top: 215vh
-                left: 87vw
+            .associate_background_images_3
+                top: -980px
+                left: -200px
+                z-index: 3
+            .associate_background_images_4
+                left: 630px
+                top: -270px
+            .associate_background_images_5
+                top: -515px
+                left: -210px
             .associate_background_images_6
-                top: -695px
-                left: 115px
+                top: -465px
+                left: 0px
                 z-index: 1
             .associate_background_images_7
-                position: absolute
-                right: 6vw
-                top: 159vh
+                left: 40px
+                top: -200px
+            .associate_background_images_8
+                left: 250px
+                top: -800px
+            .associate_background_images_9
+                left: 00px
+                top: -1010px
             .associate_background_images_10
-                position: absolute
-                left: calc((100vw - 1400px) / 2)
-                top: 50vh
+                left: 100px
+                top: -540px
             .associate_background_images_11
-                position: absolute
-                left: calc((100vw - 1250px) / 2)
-                top: 15vh
+                left: -390px
+                top: -1000px
             .associate_background_images_12
-                left: 8vw
-                top: -660px
-            .associate_background_images_13
-                left: 19vw
-                top: -35px
-            .associate_background_images_14
-                position: absolute
-                left: calc((100vw - 1100px) / 2)
-                //top: -108vh
-                top: -20px
-            .associate_background_images_15
                 display: none
+            
 
 
 
@@ -216,57 +230,64 @@ export default {
             width: 100%
             position: relative
             top: -20px
+            z-index: 2
             img
                 width: 100%
         .associate_background_images
+            position: absolute
+            width: 100%
+            height: 0px
             img
                 position: relative
+                display: none
             .associate_background_images_1
-                top: 0px
-                left: 950px
+                position: absolute
+                display: block
+                top: -720px
+                right: 10vw
                 z-index: 2
             .associate_background_images_2
-                left: -120px
-                top: -200px
-            .associate_background_images_3
-                top: -400px
-                left: 154px
-            .associate_background_images_4
-                top: -270px
-                z-index: 1 
-                left: 220px
-            .associate_background_images_5
                 position: absolute
+                display: block
+                right: 15vw
+                top: -1520px
                 z-index: 2
-                top: 215vh
-                left: 87vw
+            .associate_background_images_3
+                top: -980px
+                left: -200px
+                z-index: 3
+            .associate_background_images_4
+                left: 630px
+                top: -270px
+            .associate_background_images_5
+                display: block
+                top: -15px
+                left: 34vw
             .associate_background_images_6
-                top: -695px
-                left: 115px
+                display: block
+                top: -155px
+                left: 36vw
                 z-index: 1
             .associate_background_images_7
-                position: absolute
-                right: 6vw
-                top: 159vh
+                left: 40px
+                top: -200px
+            .associate_background_images_8
+                left: 250px
+                top: -800px
+            .associate_background_images_9
+                left: 00px
+                top: -1010px
             .associate_background_images_10
-                position: absolute
-                left: calc((100vw - 1400px) / 2)
-                top: 50vh
+                left: 100px
+                top: -540px
             .associate_background_images_11
-                position: absolute
-                left: calc((100vw - 1250px) / 2)
-                top: 15vh
+                display: block
+                left: 82vw
+                top: -1100px
             .associate_background_images_12
-                left: 8vw
-                top: -660px
-            .associate_background_images_13
-                left: 19vw
-                top: -35px
-            .associate_background_images_14
+                display: block
                 position: absolute
-                left: calc((100vw - 1100px) / 2)
-                //top: -108vh
-                top: -20px
-            .associate_background_images_15
-                display: none
+                right: 0
+                top: -1070px
+            
 </style>

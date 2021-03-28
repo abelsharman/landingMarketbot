bcfd653eaa549a4d26ebb834efd2037f17bc7d04
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2021-03-28 22:11:48 +0600

    touch event

--- src/components/Examples.vue
+++ src/components/Examples.vue
@@ -140,7 +140,8 @@
             <img class="opportunities_slider_arrow" style="left: 50vw;" v-if="this.iSlider != 1" @click="clickOnSliderMinus" src="../assets/left_arrow.png" alt="left_arrow">
             <img class="opportunities_slider_arrow" style="left: 50vw;" v-if="this.iSlider == 1" src="../assets/left_arrow_passive.png" alt="left_arrow">
             <img class="opportunities_slider_arrow" style="left: 70vw;" @click="clickOnSliderPlus" src="../assets/right_arrow.png" alt="right_arrow">
-            <div class="company_slider_item company_slider_1" id="1" ref="i1">
+
+            <div class="company_slider_item company_slider_1" id="1" ref="i1" v-touch:swipe.left="clickOnSliderPlus">
                 <img class="company_slider_big_img" src="../assets/img-3 mobile.png" alt="client">
                 <div class="company_slider_text">
                     <h1>Отправка файлов и каталогов</h1>
@@ -148,7 +149,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_2" id="2" ref="i2">
+            <div class="company_slider_item company_slider_2" id="2" ref="i2" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img" src="../assets/img-3-2 mobile.png" alt="client">
                 <div class="company_slider_text">
                     <h1>Регистрация на мероприятия</h1>
@@ -156,7 +157,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_3" id="3" ref="i3">
+            <div class="company_slider_item company_slider_3" id="3" ref="i3"  v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-3 mobile.png" alt="client">
                 <div class="company_slider_text">
                     <h1>Расчет параметров</h1>
@@ -164,7 +165,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_4" id="4" ref="i4">
+            <div class="company_slider_item company_slider_4" id="4" ref="i4"  v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-4 mobile.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Активация промокодов</h1>
@@ -173,7 +174,7 @@
             </div>
 
 
-            <div class="company_slider_item company_slider_5" id="5" ref="i5">
+            <div class="company_slider_item company_slider_5" id="5" ref="i5" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-5.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Генерация паролей</h1>
@@ -181,7 +182,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_6" id="6" ref="i6">
+            <div class="company_slider_item company_slider_6" id="6" ref="i6" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-6 mobile.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Подтверждение бронирования</h1>
@@ -189,7 +190,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_7" id="7" ref="i7">
+            <div class="company_slider_item company_slider_7" id="7" ref="i7" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-7 mobile.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Сбор отзывов и обратной связи</h1>
@@ -197,7 +198,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_8" id="8" ref="i8">
+            <div class="company_slider_item company_slider_8" id="8" ref="i8" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-8 mobile.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Проведение опросов</h1>
@@ -205,7 +206,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_9" id="9" ref="i9">
+            <div class="company_slider_item company_slider_9" id="9" ref="i9" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-9 mobile.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Проведение обучения</h1>
@@ -213,7 +214,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_10" id="10" ref="i10">
+            <div class="company_slider_item company_slider_10" id="10" ref="i10" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-10 mobile.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Прием заказов</h1>
@@ -221,7 +222,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_11" id="11" ref="i11">
+            <div class="company_slider_item company_slider_11" id="11" ref="i11" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-11.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Прием платежей</h1>
@@ -229,7 +230,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_12" id="12" ref="i12">
+            <div class="company_slider_item company_slider_12" id="12" ref="i12" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-12.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Информирование и отправка уведомлений</h1>
@@ -237,7 +238,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_13" id="13" ref="i13">
+            <div class="company_slider_item company_slider_13" id="13" ref="i13" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-13.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Анкетирование</h1>
@@ -245,7 +246,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_14" id="14" ref="i14">
+            <div class="company_slider_item company_slider_14" id="14" ref="i14" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-14.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Ответы на частые вопросы</h1>
@@ -253,7 +254,7 @@
                 </div>
             </div>
 
-            <div class="company_slider_item company_slider_15" id="15" ref="i15">
+            <div class="company_slider_item company_slider_15" id="15" ref="i15" v-touch:swipe.left="clickOnSliderPlus" v-touch:swipe.right="clickOnSliderMinus">
                 <img class="company_slider_big_img"  src="../assets/img-3-15.png" alt="client" loading="lazy">
                 <div class="company_slider_text">
                     <h1>Напоминания о записи</h1>

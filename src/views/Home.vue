1d5afc753302ec927aee3859c4a94848722ef47d
Tlek Suyerbassov <tleksuerbasov@mail.ru>
2021-04-01 22:21:53 +0600

    new: load lazy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,6 +52,73 @@ export default {
 
 
 
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-Regular.woff')
+    font-weight: normal
+    font-style: normal
+
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-Light.woff')
+    font-weight: 300
+    font-style: normal
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-BoldItalic.woff')
+    font-weight: bold
+    font-style: italic
+
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-Black.woff')
+    font-weight: 900
+    font-style: normal
+
+
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-Medium.woff')
+    font-weight: 500
+    font-style: normal
+
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-Bold.woff')
+    font-weight: bold
+    font-style: normal
+
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-Thin.woff')
+    font-weight: 100
+    font-style: normal
+    
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-MediumItalic.woff')
+    font-weight: 500
+    font-style: italic
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-LightItalic.woff')
+    font-weight: 300
+    font-style: italic
+
+
+@font-face 
+    font-family: 'Cera Pro'
+    src: url('../assets/CeraPro-ThinItalic.woff')
+    font-weight: 100
+    font-style: italic
 @media screen and (min-width:1200px)
   $padddingForSite: calc((100vw - 1200px) / 2)
   .page
